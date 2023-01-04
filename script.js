c09d4ff3ebb1b416de24b0f10cb1df976a6903ec
Usama Ahmed <usamaahmed332675@gmail.com>
2023-01-04 00:13:39 -0800

    created some functions

---
+++ script.js
@@ -0,0 +1,31 @@
+let username=prompt("Enter your name");
+alert("Hello" + username);
+
+
+function addition(){
+    let num1 = Number(prompt("Enter first value"));
+    let num2 = Number(prompt("Enter second value"));
+
+    console.log(num1 + num2);
+}
+
+function subtraction(){
+    let num1 =prompt("Enter first value");
+    let num2 =prompt("Enter second value");
+
+    console.log (num1 - num2);
+}
+
+function multiplication(){
+    let num1 =prompt("Enter first value");
+    let num2 =prompt("Enter second value");
+
+    console.log (num1 * num2);
+}
+
+function division(){
+    let num1 =prompt("Enter first value");
+    let num2 =prompt("Enter second value");
+
+    console.log (num1 / num2);
+}
